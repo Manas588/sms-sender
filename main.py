c328ba2876bf6ee9df10902a2e5043dd30a92f36
Manas Kulkarni <61631964+Manas588@users.noreply.github.com>
2020-05-21 16:47:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC2817dce9debc0797241dd726651f75e5'
-auth_token = '17a7324036f9c49142bf2cf6d12a6a3a'
+account_sid = '###'
+auth_token = '####'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
                      body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+12034578103',
-                     to='+919819971211'
+                     from_='your twilio  number',
+                     to='your number'
                  )
 
 print(message.sid)
